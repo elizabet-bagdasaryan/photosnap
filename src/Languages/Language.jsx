2b98b5964e13a6ef63ad83c58946fd4bc29b5050
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-06-15 17:44:04 +0400

    fix: fix img import

--- src/Languages/Language.jsx
+++ src/Languages/Language.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import EnglishFlag from "./assets/uk.png";
-import GeorgianFlag from "./assets/geo.png";
+import EnglishFlag from "../assets/uk.png";
+import GeorgianFlag from "../assets/geo.png";
 import "./Language.css";
 
 const Language = () => {
